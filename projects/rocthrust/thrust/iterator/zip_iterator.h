67b03ad41ca772d999d213554e1ef74022e08e3e
Yung-sheng Tu <yung-sheng@streamhpc.com>
2025-10-24 13:12:29 +0000

    [thrust, docs] Use the variadic overload of make_zip_iterator in the zip_iterator docs

--- projects/rocthrust/thrust/iterator/zip_iterator.h
+++ projects/rocthrust/thrust/iterator/zip_iterator.h
@@ -457,9 +457,9 @@ tuple_host_device_transform(const Tuple& t, UnaryFunction f)
 //!   thrust::device_vector<int> int_in{0, 1, 2}, int_out(3);
 //!   thrust::device_vector<float> float_in{0.0f, 10.0f, 20.0f}, float_out(3);
 //!
-//!   thrust::copy(thrust::make_zip_iterator(thrust::make_tuple(int_in.begin(), float_in.begin())),
-//!                thrust::make_zip_iterator(thrust::make_tuple(int_in.end(),   float_in.end())),
-//!                thrust::make_zip_iterator(thrust::make_tuple(int_out.begin(),float_out.begin())));
+//!   thrust::copy(thrust::make_zip_iterator(int_in.begin(), float_in.begin()),
+//!                thrust::make_zip_iterator(int_in.end(),   float_in.end()),
+//!                thrust::make_zip_iterator(int_out.begin(),float_out.begin()));
 //!
 //!   // int_out is now [0, 1, 2]
 //!   // float_out is now [0.0f, 10.0f, 20.0f]
