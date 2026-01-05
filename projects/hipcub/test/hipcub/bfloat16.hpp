2d762a27fd8fe7c4eb4975464377ae13862acc73
Mátyás Aradi <matyas@streamhpc.com>
2026-01-05 10:10:48 +0000

    Update copyright year

--- projects/hipcub/test/hipcub/bfloat16.hpp
+++ projects/hipcub/test/hipcub/bfloat16.hpp
@@ -1,5 +1,6 @@
 /******************************************************************************
  * Copyright (c) 2021, NVIDIA CORPORATION.  All rights reserved.
+ * Modifications Copyright (c) 2026, Advanced Micro Devices, Inc.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
