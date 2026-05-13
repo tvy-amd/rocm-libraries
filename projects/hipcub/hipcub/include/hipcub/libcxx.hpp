8e19f703bbd80ba9e856733eba824d7ed22ec7b1
Beatriz Navidad Vilches <beatriz@streamhpc.com>
2026-05-13 15:51:31 +0000

    Handle upcoming vector type change

--- projects/hipcub/hipcub/include/hipcub/libcxx.hpp
+++ projects/hipcub/hipcub/include/hipcub/libcxx.hpp
@@ -98,6 +98,73 @@
     #define _HIPCUB_STD_NAMESPACE_END }
 #endif
 
+#if _HIPCUB_HAS_DEVICE_SYSTEM_STD
+
+#include _HIPCUB_STD_INCLUDE(__cccl/cuda_toolkit.h)
+#define HIPCUB_CTK_AT_LEAST(...) _CCCL_CTK_AT_LEAST(__VA_ARGS__)
+
+#else
+
+#if defined(__CUDACC__) || defined(_NVHPC_CUDA)
+#define HIPCUB_CUDA_COMPILATION() 1
+#else 
+#define HIPCUB_CUDA_COMPILATION() 0
+#endif // defined(__CUDACC__) || defined(_NVHPC_CUDA)
+
+#if HIPCUB_CUDA_COMPILATION() || HIPCUB_HAS_INCLUDE(<cuda_runtime_api.h>)
+#define HIPCUB_HAS_CTK() 1
+#else 
+#define HIPCUB_HAS_CTK() 0
+#endif // HIPCUB_CUDA_COMPILATION() || HIPCUB_HAS_INCLUDE(<cuda_runtime_api.h>)
+
+// CUDA compilers preinclude cuda_runtime.h, so we need to include it here to get the CUDART_VERSION macro
+#if HIPCUB_HAS_CTK() && !HIPCUB_CUDA_COMPILATION()
+#include <cuda_runtime_api.h>
+#endif // HIPCUB_HAS_CTK() && !HIPCUB_CUDA_COMPILATION()
+
+// Check compatibility of the CUDA compiler and CUDA toolkit headers
+#define HIPCUB_VERSION_MAJOR_INVALID -1
+#define HIPCUB_MAKE_VERSION(_PREFIX, _PAIR) \
+    (HIPCUB_PP_EVAL(HIPCUB_PP_CAT(_PREFIX, MAKE_VERSION), HIPCUB_PP_EXPAND _PAIR))
+#define HIPCUB_VERSION_IS_INVALID(_PAIR) \
+    (HIPCUB_VERSION_MAJOR == HIPCUB_VERSION_MAJOR_INVALID)
+#define HIPCUB_VERSION_COMPARE_1(_PREFIX, _VER) (!HIPCUB_VERSION_IS_INVALID(_VER()))
+#define HIPCUB_VERSION_COMPARE_3(_PREFIX, _VER, _OP, _MAJOR) \
+    (!HIPCUB_VERSION_IS_INVALID(_VER()) && (HIPCUB_VERSION_MAJOR(_VER()) _OP _MAJOR))
+#define HIPCUB_VERSION_COMPARE_4(_PREFIX, _VER, _OP, _MAJOR, _MINOR) \
+    (!HIPCUB_VERSION_IS_INVALID(_VER())                                \
+     && (HIPCUB_MAKE_VERSION(_PREFIX, _VER()) _OP HIPCUB_MAKE_VERSION(_PREFIX, (_MAJOR, _MINOR))))
+#define HIPCUB_VERSION_SELECT_COUNT(_ARG1, _ARG2, _ARG3, _ARG4, _ARG5, ...) _ARG5
+#define HIPCUB_VERSION_SELECT2(_ARGS)                                       HIPCUB_VERSION_SELECT_COUNT _ARGS
+#define HIPCUB_VERSION_SELECT(...)         \
+    HIPCUB_VERSION_SELECT2(                  \
+      (__VA_ARGS__,                          \
+       HIPCUB_VERSION_COMPARE_4,             \
+       HIPCUB_VERSION_COMPARE_3,             \
+       HIPCUB_VERSION_COMPARE_BAD_ARG_COUNT, \
+       HIPCUB_VERSION_COMPARE_1,             \
+       HIPCUB_VERSION_COMPARE_BAD_ARG_COUNT))
+#define HIPCUB_VERSION_COMPARE(_PREFIX, ...) HIPCUB_VERSION_SELECT(__VA_ARGS__)(_PREFIX, __VA_ARGS__)
+#define HIPCUB_CUDACC_EQUAL(...) HIPCUB_VERSION_COMPARE(HIPCUB_CUDACC_, HIPCUB_CUDACC, ==, __VA_ARGS__)
+
+#if HIPCUB_CUDA_COMPILATION()
+#if !HIPCUB_CUDACC_EQUAL((CUDART_VERSION / 1000), (CUDART_VERSION % 1000) / 10)
+#error "CUDA compiler and CUDA toolkit headers are incompatible, please check your include paths"
+#endif // !HIPCUB_CUDACC_EQUAL((CUDART_VERSION / 1000), (CUDART_VERSION % 1000) / 10)
+#endif // HIPCUB_CUDA_COMPILATION()
+
+#define HIPCUB_PP_CAT(a, b)         _HIPCUB_PP_CAT_IMPL(a, b)
+#define _HIPCUB_PP_CAT_IMPL(a, b)   a ## b
+#define HIPCUB_PP_EVAL(expand, arg) expand(arg)
+#define HIPCUB_PP_EXPAND(...)       __VA_ARGS__
+#define HIPCUB_VERSION_COMPARE_BAD_ARG_COUNT(...) 0
+
+#define HIPCUB_CTK_MAKE_VERSION(_MAJOR, _MINOR) ((_MAJOR) * 1000 + (_MINOR) * 10)
+#define HIPCUB_CTK()                            (CUDART_VERSION / 1000, (CUDART_VERSION % 1000) / 10)
+#define HIPCUB_CTK_AT_LEAST(...)                HIPCUB_HAS_CTK() && HIPCUB_VERSION_COMPARE(HIPCUB_CTK_, HIPCUB_CTK, >=, __VA_ARGS__)
+
+#endif // _HIPCUB_HAS_DEVICE_SYSTEM_STD
+
 // clang-format on
 
 #endif // HIPCUB_LIBCXX_HPP_
