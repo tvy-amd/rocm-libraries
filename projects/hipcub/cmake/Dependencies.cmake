05370da95eda2f8042d83a5581e67be563a919f9
Beatriz Navidad Vilches <beatriz@streamhpc.com>
2026-05-18 12:23:30 +0000

    Bump main to 3.1.1

--- projects/hipcub/cmake/Dependencies.cmake
+++ projects/hipcub/cmake/Dependencies.cmake
@@ -373,7 +373,7 @@ endif(USER_BUILD_BENCHMARK)
 
 # CUB (only for CUDA platform)
 if(HIP_COMPILER STREQUAL "nvcc")
-  set(CCCL_MINIMUM_VERSION 3.1.0)
+  set(CCCL_MINIMUM_VERSION 3.1.1)
   if(NOT DOWNLOAD_CUB)
     find_package(CCCL ${CCCL_MINIMUM_VERSION} CONFIG)
   endif()
