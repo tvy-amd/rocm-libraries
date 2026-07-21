9e564f6d8f2b0f5367d538490ea57142cc1b19ea
Josh.Chang <josh.chang@amd.com>
2026-07-21 03:28:00 +0000

    Allow VGPR overflow in ForceGenerateKernel

--- projects/hipblaslt/tensilelite/Tensile/KernelWriter.py
+++ projects/hipblaslt/tensilelite/Tensile/KernelWriter.py
@@ -9036,7 +9036,7 @@ class KernelWriter(metaclass=abc.ABCMeta):
 
       self.states.totalVgprs = max(vgprIdx, self.states.c.numVgprValu)
       if self.states.totalVgprs < 0 or self.states.totalVgprs > self.states.regCaps["MaxVgpr"]:
-        raise RuntimeError("Generating asm kernel error: total vgpr: %u not in [0, %u].\n" % (self.states.totalVgprs, self.states.regCaps["MaxVgpr"]))
+        print("warning: total VGPRS (%d) overflowed max VGPRS (%d)." % (self.states.totalVgprs, self.states.regCaps["MaxVgpr"]))
 
       agprLimit = self.states.regCaps["PhysicalMaxVgpr"] - self.states.regCaps["MaxVgpr"]
       if self.states.totalAgprs > agprLimit:
